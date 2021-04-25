f36400ac0af6cd10f0c5f560dca0ae66523f7924
Oscar Charles <oscarjamescharles@gmail.com>
2021-04-25 14:17:02 +0100

    init

---
+++ b1_3_hello_cargo/target/debug/b1_3_hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Oscar\OneDrive\UCL\learning\translacean-to-rustacean\b1_3_hello_cargo\target\debug\b1_3_hello_cargo.exe: C:\Oscar\OneDrive\UCL\learning\translacean-to-rustacean\b1_3_hello_cargo\src\main.rs
---
+++ b1_3_hello_cargo/target/debug/deps/b1_3_hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Oscar\OneDrive\UCL\learning\translacean-to-rustacean\b1_3_hello_cargo\target\debug\deps\b1_3_hello_cargo.exe: src\main.rs
+
+C:\Oscar\OneDrive\UCL\learning\translacean-to-rustacean\b1_3_hello_cargo\target\debug\deps\b1_3_hello_cargo.d: src\main.rs
+
+src\main.rs:
